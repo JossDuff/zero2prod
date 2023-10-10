51da72bbc2ca0b2232eb7e3f5f67bfd283997238
jossduff <jduff360@gmail.com>
2023-10-09 23:04:04 -0400

    Done with chapter 3

--- src/startup.rs
+++ src/startup.rs
@@ -1,19 +1,23 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 // Notice the different signature!
 // We return `Server` on the happy path and we dropped the `async` keyword
 // We have no .await call, so it is not needed anymore.
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    // Wrap the connection in a smart pointer
+    let db_pool = web::Data::new(db_pool);
+    // Capture `connection` from the surrounding environment
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
-    // No .await here!
     Ok(server)
 }
