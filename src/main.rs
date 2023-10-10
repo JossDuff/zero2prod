12190d7ea10ded463699ca736f2846d199d9c4bf
jossduff <jduff360@gmail.com>
2023-10-09 21:39:43 -0400

    added .env

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 //! main.rs
 
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    // We have removed the hard-coded port.  Now it's coming from our settings!
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
