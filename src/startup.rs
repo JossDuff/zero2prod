c57a1571a15d18aa280f685c415213ee9c6b3dcd
jossduff <jduff360@gmail.com>
2023-10-10 13:51:17 -0400

    up to 4.5.9

--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,7 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
+use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -13,6 +15,8 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     // Capture `connection` from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
+            // Middlewares are using the `wrap` method on `App`
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
